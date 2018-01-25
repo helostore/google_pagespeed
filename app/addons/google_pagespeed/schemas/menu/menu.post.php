2c9b27df3d956a7d8c529f90d4da0081901b7a1e
WSergio <sergiu@disruptive.academy>
2018-01-25 20:36:59 +0200

    Bumped version to 1.0.1

---
+++ app/addons/google_pagespeed/schemas/menu/menu.post.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * HELOstore
+ *
+ * This source file is part of a commercial software. Only users who have purchased a valid license through
+ * https://helostore.com/ and accepted to the terms of the License Agreement can install this product.
+ *
+ * @category   Add-ons
+ * @package    HELOstore
+ * @copyright  Copyright (c) 2018 HELOstore. (https://helostore.com/)
+ * @license    https://helostore.com/legal/license-agreement/   License Agreement
+ * @version    $Id$
+ */
+
+$schema['top']['administration']['items']['storage']['subitems']['google_pagespeed.clear_cache'] = array(
+    'href'     => 'google_pagespeed.clear_cache?redirect_url=%CURRENT_URL',
+    'position' => 210,
+);
+
+return $schema;
